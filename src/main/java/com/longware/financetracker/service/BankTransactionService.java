2b6c39798bb6f20a4910084773b3089966736cbb
ScottLong76 <ScottLong76@gmail.com>
2023-12-27 01:40:30 -0500

    Work in progress - OFX handling

--- src/main/java/com/longware/financetracker/service/BankTransactionService.java
+++ src/main/java/com/longware/financetracker/service/BankTransactionService.java
@@ -1,31 +1,45 @@
 package com.longware.financetracker.service;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
+import com.longware.financetracker.entities.Bank;
 import com.longware.financetracker.entities.BankTransaction;
 import com.longware.financetracker.entities.UserAccount;
 import com.longware.financetracker.repository.BankTransactionRepository;
+import com.webcohesion.ofx4j.domain.data.MessageSetType;
+import com.webcohesion.ofx4j.domain.data.ResponseEnvelope;
+import com.webcohesion.ofx4j.domain.data.ResponseMessage;
+import com.webcohesion.ofx4j.domain.data.ResponseMessageSet;
+import com.webcohesion.ofx4j.domain.data.banking.BankStatementResponseTransaction;
+import com.webcohesion.ofx4j.domain.data.common.Payee;
+import com.webcohesion.ofx4j.domain.data.common.Transaction;
+import com.webcohesion.ofx4j.domain.data.common.TransactionList;
+import com.webcohesion.ofx4j.domain.data.common.TransactionType;
+import com.webcohesion.ofx4j.domain.data.creditcard.CreditCardStatementResponseTransaction;
+import com.webcohesion.ofx4j.io.AggregateUnmarshaller;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
+import lombok.extern.java.Log;
 
-// Service Layer for BankTransaction Entity
 @Service
 @RequiredArgsConstructor
 @Getter
 @Setter
+@Log
 public class BankTransactionService {
 
     private final BankTransactionRepository bankTransactionRepository;
-
-    // Write methods to interact with all methods currently defined in the
-    // BankTransactionRepository
-    public void deleteAll() {
-        bankTransactionRepository.deleteAll();
-    }
 
     public void deleteById(Long id) {
         bankTransactionRepository.deleteById(id);
@@ -69,6 +83,104 @@ public class BankTransactionService {
 
     public Optional<BankTransaction> getEntity(BankTransaction bankTransaction) {
         return bankTransactionRepository.getEntity(bankTransaction);
+    }
+
+    public List<BankTransaction> processOFXFile(File file, Bank selectedUploadBank) {
+        List<BankTransaction> parsedTransactions = new ArrayList<BankTransaction>();
+        try {
+
+            AggregateUnmarshaller<ResponseEnvelope> unmarshaller = new AggregateUnmarshaller<ResponseEnvelope>(
+                    ResponseEnvelope.class);
+            ResponseEnvelope envelope = unmarshaller.unmarshal(new FileInputStream(file));
+
+            MessageSetType messageSetType = MessageSetType.banking;
+
+            ResponseMessageSet messageSet = (ResponseMessageSet) envelope
+                    .getMessageSet(messageSetType);
+
+            if (messageSet == null) {
+                messageSetType = MessageSetType.creditcard;
+                messageSet = (ResponseMessageSet) envelope
+                        .getMessageSet(MessageSetType.creditcard);
+            }
+
+            List<ResponseMessage> responses = messageSet.getResponseMessages();
+
+            for (ResponseMessage response : responses) {
+                TransactionList transactionList = null;
+                if (response instanceof BankStatementResponseTransaction) {
+                    transactionList = ((BankStatementResponseTransaction) response).getMessage().getTransactionList();
+                } else if (response instanceof CreditCardStatementResponseTransaction) {
+                    transactionList = ((CreditCardStatementResponseTransaction) response).getMessage()
+                            .getTransactionList();
+                } else {
+                    log.warning("Unknown response type: " + response.getClass().getName());
+                    return parsedTransactions;
+                }
+                List<Transaction> transactions = transactionList.getTransactions();
+                for (Transaction ofxTransaction : transactions) {
+                    BankTransaction transaction = new BankTransaction();
+                    String description = "";
+                    if (ofxTransaction.getMemo() != null) {
+                        description += ofxTransaction.getMemo() + " ";
+                    }
+                    transaction.setDescription(description);
+
+                    LocalDate transactionDate = null;
+                    if (ofxTransaction.getDateInitiated() != null) {
+                        transactionDate = ofxTransaction.getDateInitiated().toInstant().atZone(ZoneId.systemDefault())
+                                .toLocalDate();
+                    } else if (ofxTransaction.getDateAvailable() != null) {
+                        transactionDate = ofxTransaction.getDateAvailable().toInstant().atZone(ZoneId.systemDefault())
+                                .toLocalDate();
+                    } else if (ofxTransaction.getDatePosted() != null) {
+                        transactionDate = ofxTransaction.getDatePosted().toInstant().atZone(ZoneId.systemDefault())
+                                .toLocalDate();
+                    }
+                    transaction.setTransactionDate(transactionDate);
+                    Payee payee = ofxTransaction.getPayee();
+                    if (payee != null && payee.getName() != null) {
+                        String payeeDetails = "Name: " + payee.getName();
+                        String payeeAddress = "";
+                        if (payee.getAddress1() != null)
+                            payeeAddress += (payee.getAddress1() + " ");
+                        if (payee.getAddress2() != null)
+                            payeeAddress += (payee.getAddress2() + " ");
+                        if (payee.getAddress3() != null)
+                            payeeAddress += (payee.getAddress3() + " ");
+                        if (payee.getCity() != null)
+                            payeeAddress += (payee.getCity() + " ");
+                        if (payee.getState() != null)
+                            payeeAddress += (payee.getState() + " ");
+                        if (payee.getZip() != null)
+                            payeeAddress += (payee.getZip() + " ");
+                        if (payee.getCountry() != null)
+                            payeeAddress += (payee.getCountry() + " ");
+                        if (payee.getPhone() != null)
+                            payeeAddress += (payee.getPhone() + " ");
+                        if (payeeAddress.length() > 0) {
+                            payeeAddress = "Address: " + payeeAddress;
+                            payeeDetails += (" " + payeeAddress);
+                        }
+                        transaction.setPayee(payeeDetails);
+                    }
+                    TransactionType transactionType = ofxTransaction.getTransactionType();
+                    if (transactionType != null) {
+                        transaction.setTransactionType(transactionType.toString());
+                    }
+                    parsedTransactions.add(transaction);
+                }
+                if (parsedTransactions.size() > 0) {
+                    bankTransactionRepository.saveAll(parsedTransactions);
+                }
+            }
+
+        } catch (Exception ex) {
+            log.severe("Error occurred while parsing OFX file: " + ex.getMessage());
+            log.severe("Exception details: ");
+            log.severe(Arrays.toString(ex.getStackTrace()));
+        }
+        return parsedTransactions;
     }
 
 }
---
+++ src/test/java/com/longware/financetracker/service/BankTransactionServiceTest.java
@@ -0,0 +1,87 @@
+package com.longware.financetracker.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import com.longware.financetracker.entities.BankTransaction;
+import com.longware.financetracker.entities.UserAccount;
+import com.longware.financetracker.repository.BankTransactionRepository;
+
+class BankTransactionServiceTest {
+
+    @Mock
+    private BankTransactionRepository bankTransactionRepository;
+
+    @InjectMocks
+    private BankTransactionService bankTransactionService;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    void testSave() {
+        BankTransaction bankTransaction = new BankTransaction();
+        bankTransaction.setId(1L);
+        bankTransaction.setDescription("Test Transaction");
+
+        // Save the bank transaction
+        BankTransaction savedTransaction = bankTransactionService.save(bankTransaction);
+
+        assertNotNull(savedTransaction);
+        assertEquals(bankTransaction.getId(), savedTransaction.getId());
+        assertEquals(bankTransaction.getDescription(), savedTransaction.getDescription());
+    }
+
+    @Test
+    void testFindById() {
+        Long transactionId = 1L;
+
+        // Mock the repository to return a bank transaction with the given ID
+        BankTransaction mockTransaction = new BankTransaction();
+        mockTransaction.setId(transactionId);
+        mockTransaction.setDescription("Mock Transaction");
+        Optional<BankTransaction> optionalTransaction = Optional.of(mockTransaction);
+        when(bankTransactionRepository.findById(transactionId)).thenReturn(optionalTransaction);
+
+        // Find the bank transaction by ID
+        Optional<BankTransaction> foundTransaction = bankTransactionService.findById(transactionId);
+
+        assertTrue(foundTransaction.isPresent());
+        assertEquals(transactionId, foundTransaction.get().getId());
+        assertEquals("Mock Transaction", foundTransaction.get().getDescription());
+    }
+
+    @Test
+    void testFindByUserAccount() {
+        UserAccount userAccount = new UserAccount();
+        userAccount.setId(1L);
+
+        // Mock the repository to return a list of bank transactions for the given user
+        // account
+        List<BankTransaction> mockTransactions = new ArrayList<>();
+        mockTransactions.add(new BankTransaction());
+        mockTransactions.add(new BankTransaction());
+        when(bankTransactionRepository.findByUserAccount(userAccount)).thenReturn(mockTransactions);
+
+        // Find the bank transactions by user account
+        Iterable<BankTransaction> foundTransactions = bankTransactionService.findByUserAccount(userAccount);
+
+        assertNotNull(foundTransactions);
+        assertEquals(2, ((List<BankTransaction>) foundTransactions).size());
+    }
+
+    // Add more test methods for other methods in the BankTransactionService class
+
+}
